97fcad6cb6fcf4e2334c1825784802e2d08d6666
punit355743 <punit.java.dixit@gmail.com>
2021-01-28 09:45:31 +0530

    redux implement in pizza shop app

--- src/components/PizzaShop.js
+++ src/components/PizzaShop.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { buyPizza, buy100Pizza } from './../redux/pizza/pizzaAction';
 
 function PizzaShop(props) {
     return (
         <div>
-            <h1>Avaiable Pizza in shop </h1>
-            <button>Buy a PIZZA</button>
+
+            <h1>Shop is close</h1>
+
+            <h1 style={{ background: 'red' }}>Avaiable Pizza in shop {props.numberOfPizza} </h1>
+            <button onClick={props.buyAPizza}>Buy a PIZZA</button>
+            <button onClick={props.buy100Pizza}>Buy 100 PIZZAS</button>
+            <button>Open A shop</button>
         </div>
     );
 }
 
-export default PizzaShop;
+const mapStateToProps = (state) => {
+    return {
+        numberOfPizza: state.numberOFPizza
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyAPizza: () => { dispatch(buyPizza()) },
+        buy100Pizza: () => { dispatch(buy100Pizza()) },
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PizzaShop);
+
+
